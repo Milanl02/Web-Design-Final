7d9f30c4853136c9eb529eed4af6b0c65324d7ba
Milanl02 <89406051+Milanl02@users.noreply.github.com>
2021-12-09 00:13:15 -0500

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,121 @@
+//For Hamburger Menu:
+var toggle=false;
+
+document.body.querySelector(".menuIcon").addEventListener("click", function (){
+
+    toggle=!toggle;
+
+    if(toggle){
+        document.body.querySelector(".menu").style.right="0";
+    }else{
+        document.body.querySelector(".menu").style.right="-160px";
+    }
+})
+
+//For Dodge Charger:
+var charger=false;
+
+document.body.querySelector(".carbox").addEventListener("click", function (){
+
+    charger=!charger;
+
+    if(charger) {
+        document.body.querySelector(".chargerbox").style.left = "418px";
+    }else{
+        document.body.querySelector(".chargerbox").style.left="-1000px";
+    }
+})
+
+var charger=false;
+
+document.body.querySelector(".carboxtool").addEventListener("click", function (){
+
+    charger=!charger;
+
+    if(charger) {
+        document.body.querySelector(".chargerbox").style.left = "418px";
+    }else{
+        document.body.querySelector(".chargerbox").style.left="-1000px";
+    }
+})
+
+//For Dodge Challenger:
+var charger=false;
+
+document.body.querySelector(".carbox2").addEventListener("click", function (){
+
+    charger=!charger;
+
+    if(charger) {
+        document.body.querySelector(".challengerbox").style.left = "418px";
+    }else{
+        document.body.querySelector(".challengerbox").style.left="-1000px";
+    }
+})
+
+var charger=false;
+
+document.body.querySelector(".carboxtool2").addEventListener("click", function (){
+
+    charger=!charger;
+
+    if(charger) {
+        document.body.querySelector(".challengerbox").style.left = "418px";
+    }else{
+        document.body.querySelector(".challengerbox").style.left="-1000px";
+    }
+})
+
+//For Dodge Durango:
+var charger=false;
+
+document.body.querySelector(".carbox3").addEventListener("click", function (){
+
+    charger=!charger;
+
+    if(charger) {
+        document.body.querySelector(".durangobox").style.right = "418px";
+    }else{
+        document.body.querySelector(".durangobox").style.right="-1000px";
+    }
+})
+
+var charger=false;
+
+document.body.querySelector(".carboxtool3").addEventListener("click", function (){
+
+    charger=!charger;
+
+    if(charger) {
+        document.body.querySelector(".durangobox").style.right = "418px";
+    }else{
+        document.body.querySelector(".durangobox").style.right="-1000px";
+    }
+})
+
+//For Ram TRX:
+var charger=false;
+
+document.body.querySelector(".carbox4").addEventListener("click", function (){
+
+    charger=!charger;
+
+    if(charger) {
+        document.body.querySelector(".trxbox").style.right = "418px";
+    }else{
+        document.body.querySelector(".trxbox").style.right="-1000px";
+    }
+})
+
+var charger=false;
+
+document.body.querySelector(".carboxtool4").addEventListener("click", function (){
+
+    charger=!charger;
+
+    if(charger) {
+        document.body.querySelector(".trxbox").style.right = "418px";
+    }else{
+        document.body.querySelector(".trxbox").style.right="-1000px";
+    }
+})
